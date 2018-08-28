3386803850ab6678f24e963c36327365593b7f8a
Ning Nan <nanning9925@gmail.com>
2018-08-29 02:33:52 +1000

    j

--- app/src/main/java/com/miles/foodtruck/Controller/SaveBtnOnClickListener.java
+++ app/src/main/java/com/miles/foodtruck/Controller/SaveBtnOnClickListener.java
@@ -43,28 +43,8 @@ public class SaveBtnOnClickListener implements View.OnClickListener {
 
 
 
-        TrackingService trackingService = TrackingService.getSingletonInstance(v.getContext());
-        Log.i(LOG_TAG, "Parsed File Contents:");
-        trackingService.logAll();
 
-        try
-        {
-            // 5 mins either side of 05/07/2018 1:05:00 PM
-            // PRE: make sure tracking_data.txt contains valid matches
-            // PRE: make sure device locale matches provided DateFormat (you can change either device settings or String param)
-            DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM);
-            String searchDate = "05/07/2018 1:06:00 PM";
-            int searchWindow = 10; // +/- 5 mins
-            Date date = dateFormat.parse(searchDate);
-            List<TrackingService.TrackingInfo> matched = trackingService
-                    .getTrackingInfoForTimeRange(date, searchWindow, 0);
-            Log.i(LOG_TAG, String.format("Matched Query: %s, +/-%d mins", searchDate, searchWindow));
-            trackingService.log(matched);
-        }
-        catch (ParseException e)
-        {
-            e.printStackTrace();
-        }
+
 
 
     }
--- app/src/main/java/com/miles/foodtruck/Util/Helpers.java
+++ app/src/main/java/com/miles/foodtruck/Util/Helpers.java
@@ -1,6 +1,14 @@
 package com.miles.foodtruck.Util;
 
 
+import android.util.Log;
+
+import com.miles.foodtruck.Service.TrackingService;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.List;
 
 public class Helpers {
 
@@ -46,6 +54,41 @@ public class Helpers {
 
 
         return null;
+    }
+
+    public static boolean onSubmitCheck(String title, String dateText, String timeText, String trackableId, TrackingService trackingService){
+
+        if (title.equals(""))
+        {
+
+        }
+
+
+
+
+
+        Log.i("ASD", "Parsed File Contents:");
+        trackingService.logAll();
+
+        try
+        {
+            // 5 mins either side of 05/07/2018 1:05:00 PM
+            // PRE: make sure tracking_data.txt contains valid matches
+            // PRE: make sure device locale matches provided DateFormat (you can change either device settings or String param)
+            DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM);
+            String searchDate = "05/07/2018 1:06:00 PM";
+            int searchWindow = 10; // +/- 5 mins
+            Date date = dateFormat.parse(searchDate);
+            List<TrackingService.TrackingInfo> matched = trackingService
+                    .getTrackingInfoForTimeRange(date, searchWindow, 0);
+            Log.i("SAD", String.format("Matched Query: %s, +/-%d mins", searchDate, searchWindow));
+            trackingService.log(matched);
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 
